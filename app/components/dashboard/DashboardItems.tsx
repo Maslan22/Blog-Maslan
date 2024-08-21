e4b302dd088b8e27cc8f69167cd03e5d5af2b8d7
Maslan Iddrisu <henrycoffie22@gmail.com>
2024-08-21 23:40:03 +0000

    dashboard layout works

---
+++ app/components/dashboard/DashboardItems.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { navLinks } from "@/app/dashboard/layout";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export function DashboardItems() {
+  const pathname = usePathname();
+  return (
+    <>
+      {navLinks.map((link, index) => (
+        <Link
+          href={link.href}
+          key={index}
+          className={cn(
+            pathname === link.href
+              ? "bg-muted text-primary"
+              : "text-muted-foreground bg-none",
+            "flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary/70"
+          )}
+        >
+          <link.icon className="size-4" />
+          {link.name}
+        </Link>
+      ))}
+    </>
+  );
+}
