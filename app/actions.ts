ae1d576bf9164ad7609dbdfc33c6375efc1549a9
Maslan Iddrisu <henrycoffie22@gmail.com>
2024-10-24 16:47:19 +0000

    Chore: site form validation and saving

--- app/actions.ts
+++ app/actions.ts
@@ -1,12 +1,38 @@
+"use server";
+
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
+import { parseWithZod } from "@conform-to/zod";
+import { siteSchema } from "./utils/zodSchemas";
+import prisma from "./utils/db";
 
-export async function CreateSiteAction(){
-    const {getUser} = getKindeServerSession();
-
+export async function CreateSiteAction(prevState: any, formData: FormData) {
+    const { getUser } = getKindeServerSession();
     const user = await getUser();
 
-    if(!user){
-        return redirect("api/auth/login");
+    if (!user) {
+        await prisma.$disconnect();
+        return redirect("/api/auth/login");
     }
+
+    const submission = parseWithZod(formData, {
+        schema: siteSchema,
+    });
+
+    if (submission.status !== "success") {
+        await prisma.$disconnect();
+        return submission.reply();
+    }
+
+    const response = await prisma.site.create({
+        data: {
+            description: submission.value.description,
+            name: submission.value.name,
+            subdirectory: submission.value.subdirectory,
+            userId: user.id,
+        }
+    });
+
+    await prisma.$disconnect();
+    return redirect(`/dashboard/sites`);
 }
