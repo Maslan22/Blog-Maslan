9fc577b48bdaa8501454ac719b8679bf1f4ff198
Maslan Iddrisu <henrycoffie22@gmail.com>
2024-10-25 16:32:35 +0000

    Article page creation form implementation -- inprogress

--- app/dashboard/sites/[siteId]/page.tsx
+++ app/dashboard/sites/[siteId]/page.tsx
@@ -1,7 +1,7 @@
 import prisma from "@/app/utils/db";
 import { Button } from "@/components/ui/button";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { Book, PlusCircle, Settings } from "lucide-react";
+import { Book, FileIcon, PlusCircle, Settings } from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 
@@ -49,7 +49,7 @@ export default async function SiteIdRoute({params}: {params: {siteId: string}}) 
           </Link>
         </Button>
         <Button asChild>
-          <Link href={"#"}>
+          <Link href={`/dashboard/sites/${params.siteId}/create`}>
             <PlusCircle className="size-4 mr-2" />
             Create Article
           </Link>
@@ -57,7 +57,24 @@ export default async function SiteIdRoute({params}: {params: {siteId: string}}) 
       </div>
 
       {data === undefined || data.length === 0 ? (
-        <h1>empty</h1>
+        <div className="flex flex-col items-center justify-center rounded-md border border-dashed p-8 text-center animate-in fade-in-50">
+        <div className="flex size-20 items-center justify-center rounded-full bg-primary/10">
+          <FileIcon className="size-10 text-primary" />
+        </div>
+        <h2 className="mt-6 text-xl font-semibold">
+          You don't have any Sites created
+        </h2>
+        <p className="mb-8 mt-2 text-center text-sm leading-tight text-muted-foreground max-w-sm mx-auto">
+          You don't have any Sites. Please create some to view them!
+        </p>
+
+
+        <Button asChild>
+          <Link href={"/dashboard/sites/new"}>
+            <PlusCircle className="size-4 mr-2" /> Create Site
+          </Link>
+        </Button>
+      </div>
       ):(
         <h1>here is data</h1>
       )}
