7c8eeb8b1eae273f37cfb1419ec812722919a1b8
Maslan Iddrisu <henrycoffie22@gmail.com>
2025-01-18 02:41:15 +0000

    Chore: Article table set up

--- app/dashboard/sites/[siteId]/page.tsx
+++ app/dashboard/sites/[siteId]/page.tsx
@@ -1,7 +1,39 @@
 import prisma from "@/app/utils/db";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { Book, FileIcon, PlusCircle, Settings } from "lucide-react";
+
+import {
+  Book,
+  FileIcon,
+  MoreHorizontal,
+  PlusCircle,
+  Settings,
+} from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 
@@ -24,7 +56,11 @@ async function getData(userId: string, siteId: string) {
   return data;
 }
 
-export default async function SiteIdRoute({params}: {params: {siteId: string}}) {
+export default async function SiteIdRoute({
+  params,
+}: {
+  params: { siteId: string };
+}) {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
@@ -58,25 +94,92 @@ export default async function SiteIdRoute({params}: {params: {siteId: string}}) 
 
       {data === undefined || data.length === 0 ? (
         <div className="flex flex-col items-center justify-center rounded-md border border-dashed p-8 text-center animate-in fade-in-50">
-        <div className="flex size-20 items-center justify-center rounded-full bg-primary/10">
-          <FileIcon className="size-10 text-primary" />
+          <div className="flex size-20 items-center justify-center rounded-full bg-primary/10">
+            <FileIcon className="size-10 text-primary" />
+          </div>
+          <h2 className="mt-6 text-xl font-semibold">
+            You don't have any Sites created
+          </h2>
+          <p className="mb-8 mt-2 text-center text-sm leading-tight text-muted-foreground max-w-sm mx-auto">
+            You don't have any Sites. Please create some to view them!
+          </p>
+
+          <Button asChild>
+            <Link href={"/dashboard/sites/new"}>
+              <PlusCircle className="size-4 mr-2" /> Create Site
+            </Link>
+          </Button>
         </div>
-        <h2 className="mt-6 text-xl font-semibold">
-          You don't have any Sites created
-        </h2>
-        <p className="mb-8 mt-2 text-center text-sm leading-tight text-muted-foreground max-w-sm mx-auto">
-          You don't have any Sites. Please create some to view them!
-        </p>
-
-
-        <Button asChild>
-          <Link href={"/dashboard/sites/new"}>
-            <PlusCircle className="size-4 mr-2" /> Create Site
-          </Link>
-        </Button>
-      </div>
-      ):(
-        <h1>here is data</h1>
+      ) : (
+        <div>
+          <Card>
+            <CardHeader>
+              <CardTitle>Article</CardTitle>
+              <CardDescription>
+                Manage your Articles in a simple and intuitive interface
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>Image</TableHead>
+                    <TableHead>Title</TableHead>
+                    <TableHead>Status</TableHead>
+                    <TableHead>Created At</TableHead>
+                    <TableHead className="text-right">Actions</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {data.map((item) => (
+                    <TableRow key={item.id}>
+                      <TableCell>
+                        <Image
+                          src={item.image}
+                          width={64}
+                          height={64}
+                          alt="Article Cover Image"
+                          className="size-16 rounded-md object-cover"
+                        />
+                      </TableCell>
+                      <TableCell className="font-medium">
+                        {item.title}
+                      </TableCell>
+                      <TableCell>
+                        <Badge
+                          variant="outline"
+                          className="bg-green-500/10 text-green-500"
+                        >
+                          Published
+                        </Badge>
+                      </TableCell>
+                      <TableCell>
+                        {new Intl.DateTimeFormat("en-US", {
+                          dateStyle: "medium",
+                        }).format(item.createdAt)}
+                      </TableCell>
+                      <TableCell className="text-end">
+                        <DropdownMenu>
+                          <DropdownMenuTrigger asChild>
+                            <Button size="icon" variant="ghost">
+                              <MoreHorizontal className="size-4" />
+                            </Button>
+                          </DropdownMenuTrigger>
+                          <DropdownMenuContent align="end">
+                            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem>Edit</DropdownMenuItem>
+                            <DropdownMenuItem>Delete</DropdownMenuItem>
+                          </DropdownMenuContent>
+                        </DropdownMenu>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </CardContent>
+          </Card>
+        </div>
       )}
     </>
   );
