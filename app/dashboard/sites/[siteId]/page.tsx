aa12395915902326dc52ded604638b2583c5da97
Maslan Iddrisu <henrycoffie22@gmail.com>
2025-01-18 10:46:36 +0000

    Chore: Article edit implemented

--- app/dashboard/sites/[siteId]/page.tsx
+++ app/dashboard/sites/[siteId]/page.tsx
@@ -168,7 +168,9 @@ export default async function SiteIdRoute({
                           <DropdownMenuContent align="end">
                             <DropdownMenuLabel>Actions</DropdownMenuLabel>
                             <DropdownMenuSeparator />
-                            <DropdownMenuItem>Edit</DropdownMenuItem>
+                            <DropdownMenuItem asChild>
+                              <Link href={`/dashboard/sites/${params.siteId}/${item.id}`}>Edit</Link>
+                            </DropdownMenuItem>
                             <DropdownMenuItem>Delete</DropdownMenuItem>
                           </DropdownMenuContent>
                         </DropdownMenu>
